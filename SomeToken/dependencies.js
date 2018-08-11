d0a9299e7e02bd0222fcdfa8e2521738a2a60c9a
Ubuntu <ubuntu@ip-172-31-33-67.us-east-2.compute.internal>
2018-08-11 20:03:41 +0000

    clean up deps with new zep structure

--- SomeToken/dependencies.js
+++ SomeToken/dependencies.js
@@ -2,9 +2,7 @@
 const fs = require('fs');
 module.exports = {
   'openzeppelin-solidity/contracts/math/SafeMath.sol': fs.readFileSync('openzeppelin-solidity/contracts/math/SafeMath.sol', 'utf8'),
-  'openzeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol': fs.readFileSync('openzeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol', 'utf8'),
   'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol': fs.readFileSync('openzeppelin-solidity/contracts/token/ERC20/ERC20.sol', 'utf8'),
-  'openzeppelin-solidity/contracts/token/ERC20/BasicToken.sol': fs.readFileSync('openzeppelin-solidity/contracts/token/ERC20/BasicToken.sol', 'utf8'),
   'openzeppelin-solidity/contracts/ownership/Ownable.sol': fs.readFileSync('openzeppelin-solidity/contracts/ownership/Ownable.sol', 'utf8'),
   'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol': fs.readFileSync('openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol', 'utf8'),
   'openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol': fs.readFileSync('openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol', 'utf8'),
